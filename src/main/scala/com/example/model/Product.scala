5f455946f36fb400537f71d0f66f1ab027bd8d78
akhilvijayan05 <akhilvijayan05@gmail.com>
2018-05-15 15:16:57 +0530

    Added comment in Product case class

--- src/main/scala/com/example/model/Product.scala
+++ src/main/scala/com/example/model/Product.scala
@@ -7,8 +7,8 @@ import upickle.default.{macroRW, ReadWriter}
 case class Product(brand: String, model : String)
 
 object Product {
-  implicit val playJsonFormatter  = Json.format[Product]
-  implicit def uPickleFormatter: ReadWriter[Product] = macroRW
+  implicit val playJsonFormatter  = Json.format[Product]     //For PlayJson
+  implicit def uPickleFormatter: ReadWriter[Product] = macroRW  //For uPickle
   implicit def PersonCodecJson =
-    casecodec2(Product.apply, Product.unapply)("brand", "model")
+    casecodec2(Product.apply, Product.unapply)("brand", "model")   //For Argonaut
 }
